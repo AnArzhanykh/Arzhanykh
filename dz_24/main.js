00bebd09fb4a384045e3ed69f49014367de382d0
unknown <Anarzhanykh@gmail.com>
2019-01-28 23:24:10 +0200

    24

---
+++ dz_24/main.js
@@ -0,0 +1,21 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        blocks: [
+            {name: 1, color: 'grean'},
+            {name: 2, color: 'blue'},
+            {name: 3, color: 'grean'},
+            {name: 4, color: 'orange'},
+            {name: 5, color: 'grean'},
+            {name: 6, color: 'grean'},
+            {name: 7, color: 'grean'}
+        ],
+        maxHeight: '300px',
+    },
+    methods: {
+
+
+    }
+  })
+
